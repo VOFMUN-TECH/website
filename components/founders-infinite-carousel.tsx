d5dcf00810fb961db9ed5924e12d14b8efeb7527
vofmun <vofmun@gmail.com>
2026-01-13 20:26:44 +0400

    Update founders-infinite-carousel.tsx

--- components/founders-infinite-carousel.tsx
+++ components/founders-infinite-carousel.tsx
@@ -87,6 +87,11 @@ const founders = [
     role: "Deputy of Media & Marketing",
     image: "/founders/HanxiaoYu_Media.jpg",
   },
+  {
+    name: "Saira Shirvaikar",
+    role: "Deputy of Media & Marketing",
+    image: "/founders/SairaShirvaikar_Media.jpeg",
+  },
 ]
 
 export function FoundersInfiniteCarousel() {
